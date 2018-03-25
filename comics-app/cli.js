9c8a74c7b78b19947bbf84aa8ed349638e247104
MaksAMillion <maxx852@yahoo.com>
2018-03-25 08:52:17 -0700

    deleted old code;

--- comics-app/cli.js
+++ comics-app/cli.js
@@ -4,20 +4,6 @@ const
 
 const flags = yargs.usage('$0: Usage <cmd> [options]')
     .command({
-        command: 'draw',
-        desc: 'draws a card from the deck',
-        builder: (yargs) => {
-            return yargs.option('s', {
-                alias: 'shuffle',
-                describe: 'shuffle the deck before drawing'
-            }).option('n', {
-                alias: 'number',
-                describe: 'number of cards to draw'
-            })
-        },
-        handler: (argv) => { app.draw(argv.shuffle, argv.number) }
-    })
-    .command({
         command: ('search <movie>'),
         desc: 'searches for the movie followed by the search command ',
         handler: (argv) => {app.search(argv.movie)}
